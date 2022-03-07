539c4eeda6e755cb7306e1419d2295cb2c47ac05
mdrod <mdrod@DESKTOP-H6TTJ8Q>
2022-03-07 07:21:03 -0500

    Agregar archivos de proyecto.

---
+++ Factorial de un Numero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_de_un_Numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo del Fatorial de un numero");
+            Console.WriteLine("Digite un numero: ");
+            int num = int.Parse(Console.ReadLine());
+            int Factor = 1;
+            for (int i = 1; i < num + 1; i++)
+                 Factor*= i;
+            Console.WriteLine("El factorial del numero es: " + Factor );
+            Console.ReadLine();      
+        }
+    }
+}
